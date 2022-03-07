404a1f7feac05803f503a3473f319ee8382bd8fb
Gustavo Rondello <gustavorondello99@outlook.com>
2022-03-07 15:08:01 -0300

    Requisito 1 completo

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Notfound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <p>Not Found</p>
+      </div>);
+  }
+}
+
+export default Notfound;
---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Editprofile extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        <p>Edit Profile</p>
+      </div>);
+  }
+}
+
+export default Editprofile;
