1285a8473fa8296a512225228e119c46b7695e07
Gustavo Rondello <gustavorondello99@outlook.com>
2022-03-08 16:10:13 -0300

    Requisito 2 completo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
@@ -7,17 +7,72 @@ import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
+import { createUser } from './services/userAPI';
 
-class App extends React.Component {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      load: false,
+      login: false,
+      ableLogin: true,
+    };
+  }
+
+   handleLoginName = ({ target }) => {
+     const { value } = target;
+     const minName = 3;
+     this.setState({
+       name: value,
+       ableLogin: value.length < minName,
+     });
+   } // Com ajuda da Vivi e do Vitu na mentoria consegui descobrir meu erro do async await e como utilizar para o Loading.js
+
+  onButtonClick = async (event) => {
+    event.preventDefault();
+    const { name } = this.state;
+    this.setState({ load: true });
+    await createUser({ name });
+    this.setState({
+      load: false,
+      login: true,
+    });
+  }
+
   render() {
+    const { login, name, ableLogin, load } = this.state;
     return (
       <Switch>
         <Route exact path="/profile/edit" component={ ProfileEdit } />
         <Route path="/profile" component={ Profile } />
         <Route path="/favorites" component={ Favorites } />
-        <Route exact path="/album/:id" render={ (props) => <Album { ...props } /> } />
+        <Route
+          exact
+          path="/album/:id"
+          render={ (props) => (
+            <Album
+              { ...props }
+            />) }
+        />
         <Route path="/search" component={ Search } />
-        <Route exact path="/" component={ Login } />
+        <Route
+          exact
+          path="/"
+          render={ (props) => (
+            (
+              <Login
+                { ...props }
+                load={ load }
+                name={ name }
+                login={ login }
+                ableLogin={ ableLogin }
+                onButtonClick={ this.onButtonClick }
+                handleLoginName={ this.handleLoginName }
+              />
+            )) }
+        />
+        <Route exact path="/" />
         <Route exact path="*" component={ NotFound } />
       </Switch>
     );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,49 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
 
 class Login extends Component {
   render() {
+    const { handleLoginName, onButtonClick, login, name, ableLogin, load } = this.props;
     return (
-      <div data-testid="page-login">
-        <p>Login</p>
-      </div>
+      <>
+        {load
+          ? <Loading />
+          : (
+            <div data-testid="page-login">
+              <p>Login</p>
+              <form>
+                <input
+                  value={ name }
+                  type="text"
+                  data-testid="login-name-input"
+                  onChange={ handleLoginName }
+                />
+                <button
+                  type="button"
+                  data-testid="login-submit-button"
+                  onClick={ onButtonClick }
+                  disabled={ ableLogin }
+                >
+                  Entrar
+                </button>
+              </form>
+            </div>
+          )}
+        {login && <Redirect to="/search" />}
+      </>
     );
   }
 }
+
+Login.propTypes = {
+  onButtonClick: PropTypes.func.isRequired,
+  login: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+  ableLogin: PropTypes.bool.isRequired,
+  handleLoginName: PropTypes.func.isRequired,
+  load: PropTypes.bool.isRequired,
+};
 
 export default Login;
